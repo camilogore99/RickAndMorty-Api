5686ac3798e0ef7738166d2cebd3544a4bdbb96a
camilo <camilogore99@gmail.com>
2021-02-13 12:03:23 -0500

    mejora

---
+++ src/Componentes/ResidentInfo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ResidentInfo = () => {
+      return (
+            <div classname="card col-md-6">
+                  <div className="card-body col-md-6">
+                        <div className="father col-md-6">
+                        <div className="col-md-6">
+                              aqui va la imagen 
+                        </div>
+                        <div className ="col-md-6">
+                              <span>Gonorrhea</span>
+                              <h4> dead-human</h4>
+                              <span>Last known location</span>
+                              <span>Anatomy park </span>
+                              <span>first seen </span>
+                              <span>anatomy park </span>
+                        </div>
+                        </div>
+                  </div>
+            </div>
+      )
+}
+
+export default ResidentInfo;
---
+++ src/Componentes/SearchBox.js
@@ -0,0 +1,16 @@
+import ResidentInfo from "./ResidentInfo"
+
+const SearchBox = () => {
+      return (
+            <>
+            <div className="main">
+            <h1 className="title">Rick And Morty Api</h1>
+            <input />
+            <button className="buton"> Search</button>
+            </ div>
+            <ResidentInfo />
+            </>
+      )
+}
+
+export default SearchBox;
